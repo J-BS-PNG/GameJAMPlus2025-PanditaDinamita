dbdd34e5d992d25c7448dde07cc9102efb4c4c1b
Glori <75176699+globv28@users.noreply.github.com>
2025-10-18 09:29:32 -0600

    Grid Movement
    The players moves in grid

---
+++ GJ+2025/GJ+_2025/Assets/Scripts/Movement.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Direction
+{
+    up,
+    down,
+    left,
+    right
+}
+
+public class Movement : MonoBehaviour
+{
+    Vector3 targetPosition;
+    Direction direction;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = transform.position;
+        direction = Direction.down; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            // Mientras se mueve, ignora nueva entrada
+        if (Vector3.Distance(transform.position, targetPosition) > 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+            return;
+        }
+
+        Vector2 axisDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (axisDirection != Vector2.zero)
+        {
+            if (Mathf.Abs(axisDirection.x) > Mathf.Abs(axisDirection.y))
+            {
+                if (axisDirection.x > 0)
+                {
+                    direction = Direction.right;
+                    targetPosition += Vector3.right;
+                }
+                else
+                {
+                    direction = Direction.left;
+                    targetPosition += Vector3.left;
+                }
+            }
+            else
+            {
+                if (axisDirection.y > 0)
+                {
+                    direction = Direction.up;
+                    targetPosition += Vector3.up;
+                }
+                else
+                {
+                    direction = Direction.down;
+                    targetPosition += Vector3.down;
+                }
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+    }
+}
+
+        
+
