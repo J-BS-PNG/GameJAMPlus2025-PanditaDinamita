6da838c0b1acebcd070f0d4dc68cfb00325d03d7
Josue Barrientos Sandoval <66535409+J-BS-PNG@users.noreply.github.com>
2025-10-18 16:35:09 -0600

    Habilidades proceso

--- GJ+2025/GJ+_2025/Assets/Scripts/BreakAbility.cs
+++ GJ+2025/GJ+_2025/Assets/Scripts/BreakAbility.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class BreakAbility : MonoBehaviour, IAbility
 {
-    //[SerializeField] private float breakRange = 2f;
-    //[SerializeField] private LayerMask breakableLayer;
+    [SerializeField] private int maxUses = 3;
+    private int currentUses;
+
+    void Start()
+    {
+        currentUses = maxUses;
+    }
 
     public void UseAbility() 
     {
-        Debug.Log("Romper objeto");
+        if (!canUse())
+        {
+            Debug.Log("No quedan usos para el romper");
+            return;
+        }
+
+        currentUses--;
+        Debug.Log($"Usando romper ({currentUses}/{maxUses}) restante");
+    }
+
+    public bool canUse()
+    {
+        return currentUses > 0;
+    }
+
+    public int remainingUses()
+    {
+        return currentUses;
     }
 
     public string GetName() => "Romper";
--- GJ+2025/GJ+_2025/Assets/Scripts/GrabAbility.cs
+++ GJ+2025/GJ+_2025/Assets/Scripts/GrabAbility.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 
 public class GrabAbility : MonoBehaviour, IAbility
 {
+    [SerializeField] private int maxUses = 3;
+    private int currentUses;
+
+    void Start()
+    {
+        currentUses = maxUses;
+    }
+
     public void UseAbility()
     {
-        Debug.Log("Agarrar objeto");
+        if (!canUse())
+        {
+            Debug.Log("No quedan usos para el Agarrar");
+            return;
+        }
+
+        currentUses--;
+        Debug.Log($"Usando Agarrar ({currentUses}/{maxUses}) restante");
+    }
+
+    public bool canUse()
+    {
+        return currentUses > 0;
+    }
+
+    public int remainingUses()
+    {
+        return currentUses;
     }
 
     public string GetName() => "Agarrar";
