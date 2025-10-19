d6c36eb6bc3579a38592bd46f37542edd73a28df
Glori <75176699+globv28@users.noreply.github.com>
2025-10-18 21:56:23 -0600

    NPC/DIALOG
    Gets object

--- GJ+2025/GJ+_2025/Assets/Scripts/Movement.cs
+++ GJ+2025/GJ+_2025/Assets/Scripts/Movement.cs
@@ -26,6 +26,8 @@ public class Movement : MonoBehaviour
     public int maxHealth = 3;
     private int currentHealth;
 
+    public bool objectObtained;
+
     public List<Image> hearts = new List<Image>();
     //public Sprite fullHeart;       // CorazÃ³n lleno
     //public Sprite emptyHeart; 
@@ -64,7 +66,7 @@ public class Movement : MonoBehaviour
             }
             else 
             {
-                Debug.Log($"{habilidadadActiva.GetName()} no tiene más usos.");
+                Debug.Log($"{habilidadadActiva.GetName()} no tiene mï¿½s usos.");
             }
 
         }
@@ -133,6 +135,11 @@ public class Movement : MonoBehaviour
         if (other.CompareTag("Enemy"))
         {
             TakeDamage(1); 
+        }
+        else if(other.CompareTag("Item"))
+        {
+            objectObtained = true;
+            Debug.Log("Object obtained!");
         }
     }
 
---
+++ GJ+2025/GJ+_2025/Assets/Scripts/NPC.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+
+    public string DialogueStart = "This is the mission beginning of the dialogue.";
+    public string DialogueEnd = "This is the mission end of the dialogue.";
+    public Movement player;
+
+    public GameObject dialogueBubblePrefab;
+    public Canvas uiCanvas;
+    public float bubbleDuration = 3f;
+
+    private GameObject activeBubble;
+
+    void Start()
+    {
+        player.objectObtained = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (player.objectObtained)
+            {
+                // Start the dialogue
+                Debug.Log("NPC: " + DialogueEnd);
+                ShowDialogue(DialogueEnd);
+            }
+            else
+            {
+                // Start the dialogue
+                Debug.Log("NPC: " + DialogueStart);
+                ShowDialogue(DialogueStart);
+            }
+        }
+    }
+
+
+    void ShowDialogue(string message)
+    {
+        if (activeBubble != null)
+            Destroy(activeBubble);
+
+        activeBubble = Instantiate(dialogueBubblePrefab, uiCanvas.transform);
+
+        TMP_Text textComponent = activeBubble.GetComponentInChildren<TMP_Text>();
+        RectTransform bubbleRect = activeBubble.GetComponent<RectTransform>();
+
+        if (textComponent != null)
+        {
+            textComponent.text = message;
+            LayoutRebuilder.ForceRebuildLayoutImmediate(bubbleRect);
+
+            Vector2 newSize = new Vector2(
+                textComponent.preferredWidth + 40f,  // margen horizontal
+                textComponent.preferredHeight + 20f  // margen vertical
+            );
+
+            bubbleRect.sizeDelta = newSize;
+        }
+
+        Vector3 worldPos = transform.position + Vector3.up * 2f;
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+        activeBubble.transform.position = screenPos;
+
+        Destroy(activeBubble, bubbleDuration);
+    }
+
+}
